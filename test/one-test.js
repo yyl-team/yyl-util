4e60ebd58933dadd678cd7282a42bf4670588926
jackness <jackness1208@qq.com>
2018-01-15 21:21:09 +0800

    test

--- test/one-test.js
+++ test/one-test.js
@@ -1,4 +1,16 @@
 'use strict';
+var chalk = require('chalk');
 var util = require('../lib/yyl-util.js');
 
-util.runNodeModule('yyl supercall', { cwd: __dirname });
+util.runCMD('cls');
+
+
+console.log(chalk.black.bgYellow(' JS   ') + chalk.black.bgWhite(' 10s '), chalk.magenta('A'), ':', '20', chalk.blue('M'), ':', 10, chalk.red('D'), ':', 0);
+console.log(chalk.white.bgMagenta(' IMG  ') + chalk.black.bgWhite(' 10s '), chalk.magenta('A'), ':', '20', chalk.blue('M'), ':', 10, chalk.red('D'), ':', 0);
+console.log(chalk.black.bgCyan(' CSS  ') + chalk.black.bgWhite(' 10s '), chalk.magenta('A'), ':', '20', chalk.blue('M'), ':', 10, chalk.red('D'), ':', 0);
+console.log(chalk.white.bgBlue(' HTML ') + chalk.black.bgWhite(' 10s '), chalk.magenta('A'), ':', '20', chalk.blue('M'), ':', 10, chalk.red('D'), ':', 0);
+console.log(chalk.white.bgRed(' REV  ') + chalk.black.bgWhite(' 10s '), chalk.magenta('A'), ':', '20', chalk.blue('M'), ':', 10, chalk.red('D'), ':', 0);
+console.log(chalk.black.bgGreen(' DONE '), chalk.green('Finished in 10123 ms'));
+
+
+
