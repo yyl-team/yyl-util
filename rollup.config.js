fb03354af39ad3699a25403085fb842def5edd1e
liudaojie <liudaojie@yy.com>
2021-05-08 11:47:48 +0800

    feat: 开发中

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import pkg from './package.json'
 import fs from 'fs'
 import path from 'path'
 import typescript from 'rollup-plugin-typescript2'
@@ -7,7 +6,7 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-node-externals'
 import json from '@rollup/plugin-json'
 
-function buildBanner(type) {
+function buildBanner({ type, pkg }) {
   return [
     '/*!',
     ` * ${pkg.name} ${type} ${pkg.version}`,
@@ -19,49 +18,53 @@ function buildBanner(type) {
 
 function makeConfig () {
   const pkgPath = path.join(__dirname, './packages')
-  const pkgNames = fs.readdirSync(pkgPath)
-  const buildPkgArr = []
+  const pkgNames = fs.readdirSync(pkgPath).filter((name) => !/^\./.test(name))
+  let buildPkgArr = []
   Object.keys(process.env).forEach((key) => {
     if (pkgNames.includes(key)) {
       buildPkgArr.push(key)
     }
   })
 
-  console.log('buildPkgArr', buildPkgArr)
-}
+  let contexts = []
+  if (!buildPkgArr.length) {
+    /** 加入根目录构建 */
+    contexts.push(__dirname)
+    buildPkgArr = pkgNames
+  }
 
-makeConfig()
+  contexts = contexts.concat(buildPkgArr.map(iPath => path.join(pkgPath, iPath)))
 
-const config = {
-  input: './src/index.ts',
-  output: [],
-  plugins: [
-    external({
-      deps: true
-    }),
-    nodeResolve({ jsnext: true }),
-    commonjs(),
-    json(),
-    typescript({
-      typescript: require('typescript')
-    })
-  ],
-  external: []
-}
-
-export default [
-  {
-    input: config.input,
-    output: [
-      {
-        file: './output/index.js',
+  return contexts
+    .filter((context) => fs.existsSync(path.join(context, 'package.json')))
+    .map((context) => ({
+      input: path.join(context, './src/index.ts'),
+      output: [{
+        file: path.join(context, './output/index.js'),
         format: 'cjs',
-        banner: buildBanner('cjs'),
+        banner: buildBanner({ type: 'cjs', pkg: require(path.join(context, 'package.json'))}),
         exports: 'named',
         sourcemap: false
-      }
-    ],
-    plugins: config.plugins,
-    external: config.external
-  }
-]
+      }],
+      plugins: [
+        external({
+          deps: true
+        }),
+        nodeResolve({ jsnext: true }),
+        commonjs(),
+        json(),
+        typescript({
+          typescript: require('typescript'),
+          tsconfigOverride: {
+            compilerOptions: {
+              declarationDir: path.join(context, './output')
+            },
+            include: ['typing/**/*', 'src/**.*'].map((iPath) => path.join(context, iPath))
+          }
+        })
+      ],
+      external: []
+    }))
+}
+
+export default makeConfig()
